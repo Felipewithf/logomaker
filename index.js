d49e3b4e6dd981ae90051bd81eebe0389d20ffd8
Felipe <felipe.sarmiento.as@gmail.com>
2023-08-02 13:28:17 -0400

    created SVG file with fs and writefile successfully and logged it on console

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { readFile, writeFile } = require('fs/promises');
 const inquirer = require("inquirer");
 // const shapes = require('/lib/shapes.js');
 
@@ -12,25 +12,48 @@ const validateInitials = async (input) => {
 inquirer
     .prompt([
         {
-            type: "list",
-            message:"What is your favorite shape?",
-            name: "shapes",
-            choices: ["circle","square","rectangule","triangle"]
-        },
-        {
-            type: "list",
-            message:"What is your color?",
-            name: "color",
-            choices: ["blue","red","yellow","green"]
-        },
-        {
             type: "input",
             message:"what are your initials? (max 3 characters)",
             name: "abc",
             validate: validateInitials
         },
+        {
+            type: "input",
+            message:"What is your text color?",
+            name: "color",
+        },
+        {
+            type: "list",
+            message:"What is your favorite shape?",
+            name: "shapes",
+            choices: ["circles","squares","rectangles","triangles"]
+        },
+        {
+            type: "input",
+            message:"What is your shape color?",
+            name: "shapesColor",
+        },
+        
+        
 
     ]).then((res)=>{
 
         console.log(res);
+
+        const svgdata = generateSVG();
+
+        writeFile("./examples/logo.svg", svgdata)
+        .then(()=>{
+            console.log('Success svg logo created');
+        })
+        
     });
+
+
+
+function generateSVG(){
+   const finalsvg =  `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+   <polygon points="150, 18 244, 182 56, 182" fill="Blue" />
+   <text x="150" y="125" font-size="60" text-anchor="middle" fill="Yellow">ABC</text></svg>`
+    return finalsvg;
+}
