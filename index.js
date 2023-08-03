84d6b62d1bcc80ca1615d2beaf1a41a7ec1309b3
Felipe <felipe.sarmiento.as@gmail.com>
2023-08-02 21:52:38 -0400

    SVG shape (triangle only) generated from user input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { readFile, writeFile } = require('fs/promises');
 const inquirer = require("inquirer");
-// const shapes = require('/lib/shapes.js');
+const Shapes = require('./lib/shapes.js');
+const Triangle = require('./lib/shapes.js');
 
 const validateInitials = async (input) => {
     if (input.length > 3) {
@@ -26,21 +27,18 @@ inquirer
             type: "list",
             message:"What is your favorite shape?",
             name: "shapes",
-            choices: ["circles","squares","rectangles","triangles"]
+            choices: ["Circle","Square","Rectangle","Triangle"]
         },
         {
             type: "input",
             message:"What is your shape color?",
             name: "shapesColor",
-        },
-        
-        
-
+        }
     ]).then((res)=>{
 
         console.log(res);
 
-        const svgdata = generateSVG();
+        const svgdata = generateSVG(res.shapesColor, res.shapes,res.color, res.abc);
 
         writeFile("./examples/logo.svg", svgdata)
         .then(()=>{
@@ -51,9 +49,14 @@ inquirer
 
 
 
-function generateSVG(){
-   const finalsvg =  `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-   <polygon points="150, 18 244, 182 56, 182" fill="Blue" />
-   <text x="150" y="125" font-size="60" text-anchor="middle" fill="Yellow">ABC</text></svg>`
-    return finalsvg;
+function generateSVG(s_color, shapeSelected, t_color, initials){
+
+   const svgHeader = new Shapes().renderSVG_Header();
+   const svgPaths = new Triangle().renderSVG_Paths(s_color);
+
+   const finalsvg =  `<svg ${svgHeader}>
+   ${svgPaths}
+   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${t_color}">${initials}</text></svg>`
+    
+   return finalsvg;
 }
